5c3fc964016ec3045c2e0359b6f9fccee3974a00
Koushik <koushikbabu003@gmail.com>
2024-09-08 19:24:36 +0530

    refined again

--- sapp/sapp/settings.py
+++ sapp/sapp/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
 ]
 
 # Application definition
@@ -41,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'backend',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
     'corsheaders'
 ]
 
