104d714e172107a953d90e21b61163ed7720be2a
Koushik <koushikbabu003@gmail.com>
2024-10-03 12:12:32 +0530

    added init.py

--- sapp/backend/views/users_views.py
+++ sapp/backend/views/users_views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from serializers.user_serializers import StudentsDBSerializer, FacultyDBSerializer
+from serializers.user_serializers import StudentsDBSerializer, FacultyDBSerializer, UserSerializer
 from models.user_models import StudentsDB, FacultyDB
 from utils.pagination import CustomPagination
 from utils.utils import BaseDBView
