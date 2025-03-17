d5c3b235fbe43f32f68d6912bccac28a89f0272e
Harini S <harinis7102004@gmail.com>
2025-03-17 19:29:16 +0530

    Corrected results file

--- sapp/features/veiws/results.py
+++ sapp/features/veiws/results.py
@@ -2,6 +2,8 @@ from rest_framework import generics, permissions
 from rest_framework.permissions import IsAuthenticated
 from django.db.models import Q
 from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework import status
 
 from features.models import Result, ResultLock
 from features.serializers import ResultSerializer, ResultLockSerializer
